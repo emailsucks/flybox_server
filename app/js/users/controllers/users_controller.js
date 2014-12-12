2af9781327b5187b610b711927b523bf949358e5
Charles Renwick <crenwick@crenwick-mac.local>
2014-12-11 17:26:47 -0800

    user log in mocked out

--- app/js/users/controllers/users_controller.js
+++ app/js/users/controllers/users_controller.js
@@ -3,11 +3,17 @@
 module.exports = function(app) {
   app.controller('UsersCtrl', ['$scope', function($scope) {
     $scope.errors = [];
-    console.log('usrs controller');
     $scope.signingUp = false;
+    $scope.signingIn = false;
     $scope.signUp = function() {
       console.log('signing up');
       $scope.signingUp = true;
+      $scope.signingIn = false;
+    }
+    $scope.logIn = function() {
+      console.log('signing in');
+      $scope.signingIn = true;
+      $scope.signingUp = false;
     }
   }]);
 };
