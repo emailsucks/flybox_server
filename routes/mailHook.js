2f5bce7b843b746e9c235f66f2093e108213d49b
Mark Harrell <mark@mjharrell.com>
2014-12-15 12:23:45 -0800

    pull user info for sending

--- routes/mailHook.js
+++ routes/mailHook.js
@@ -48,18 +48,18 @@ module.exports = function(app) {
         if (data === null) console.log('data is null');
         else {
           var random = '';
+          mailOptions.to = parsedEmails[i];
+          userOptions.host = data.smtp.host;
+          userOptions.port = data.smtp.port;
+          userOptions.auth.user = data.smtp.username;
+          userOptions.auth.pass = data.smtp.password;
+          userOptions.secure = data.smtp.secure;
+          mailOptions.from = userEmail;
+          var transporter = nodemailer.createTransport(userOptions);
           for (var i = 0; i < parsedEmails.length; i++) {
             random = Math.floor((Math.random() * 100000) + 1);
-            mailOptions.to = parsedEmails[i];
-            userOptions.host = data.smtp.host;
-            userOptions.port = data.smtp.port;
-            userOptions.auth.user = data.smtp.username;
-            userOptions.auth.pass = data.smtp.password;
-            userOptions.secure = data.smtp.secure;
-            mailOptions.from = userEmail;
             mailOptions.text = 'Hello world and some random numbers: ' + random;
             mailOptions.html = '<b>Hello world and some random text: </b> ' + random;
-            var transporter = nodemailer.createTransport(userOptions);
             transporter.sendMail(mailOptions, emailCallback);
           }
         }
