b1effad58b52ef548f57cda3801aa0bb33ea5bc6
Mark Harrell <mark@mjharrell.com>
2014-12-16 15:38:59 -0800

    inspecting fields

--- routes/mailHook.js
+++ routes/mailHook.js
@@ -41,7 +41,7 @@ module.exports = function(app) {
     var form = new multiparty.Form();
     form.parse(req, function(err, fields, files) {
       res.set('Content-Type', 'text/plain');
-      console.log(files);
+      console.log(fields);
       res.status(200);
       var jsonParsed = JSON.parse(fields.mailinMsg);
       res.end(util.inspect({fields: fields.mailinMsg, files: files}));
