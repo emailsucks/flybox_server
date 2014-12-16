1883b709098c39f772f9851a51ff9414e42f0124
Mark Harrell <mark@mjharrell.com>
2014-12-15 19:12:54 -0800

    add gem install to post script

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+gem install sass
 npm install grunt-contrib-jshint grunt-jscs grunt-mongo-drop grunt-simple-mocha grunt-contrib-clean grunt-contrib-copy grunt-contrib-sass grunt-browserify grunt-express-server grunt-contrib-watch
 ./node_modules/bower/bin/bower install
 ./node_modules/grunt-cli/bin/grunt build
