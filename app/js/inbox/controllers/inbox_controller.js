72c2e765ff0b73659388dc3357130fe43e8ed50e
Charles Renwick <crenwick@crenwick-mac.local>
2014-12-16 15:29:47 -0800

    added backend GET /api/userSMTP and tests

--- app/js/inbox/controllers/inbox_controller.js
+++ app/js/inbox/controllers/inbox_controller.js
@@ -19,5 +19,28 @@ module.exports = function(app) {
     $scope.goToInbox = function() {
       return $location.path('/inbox');
     };
+
+    $scope.saveSettings = function() {
+      $scope.errors = null;
+      if (!$scope.smtp.host) $scope.errors = 'please enter a host\n';
+      if (!$scope.smtp.port) $scope.errors = 'please enter a port\n';
+      if (!$scope.smtp.username) $scope.errors = 'please enter a username\n';
+      if (!$scope.smtp.password) $scope.errors = 'please enter a password\n';
+      if ($scope.errors !== null) return;
+
+      $http({
+        method: 'POST',
+        url: '/api/userSMTP',
+        data: $scope.smtp,
+        headers: {'jwt': $cookies.jwt}
+      })
+      .success(function(data) {
+        $location.path('/inbox');
+      })
+      .error(function(data) {
+        console.log('err', data);
+        $scope.errors = data;
+      });
+    };
   }]);
 };
