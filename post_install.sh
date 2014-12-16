f7914dbb9c7803979ab013552aafd61848f978da
Mark Harrell <mark@mjharrell.com>
2014-12-15 20:22:19 -0800

    last time trying sass

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+export GEM_HOME=$PWD/gems
+mkdir -p $GEM_HOME
+gem install compass
 npm install grunt-contrib-jshint grunt-jscs grunt-mongo-drop grunt-simple-mocha grunt-contrib-clean grunt-contrib-copy grunt-contrib-sass grunt-browserify grunt-express-server grunt-contrib-watch
 ./node_modules/bower/bin/bower install
 ./node_modules/grunt-cli/bin/grunt build
