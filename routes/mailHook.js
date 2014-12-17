838fc7918042265b095a81a0d01e4bcacf6c8b07
Mark Harrell <mark@mjharrell.com>
2014-12-16 19:41:30 -0800

    console log for async issues

--- routes/mailHook.js
+++ routes/mailHook.js
@@ -129,9 +129,9 @@ module.exports = function(app) {
                   console.log('done', data);
                   console.log('s3-us-west-2.amazonaws.com/' + bucket + '/' + destPath[name]);
                   fileURLS.push(destPath[name]);
-                  callback();
                 });
               }
+              callback();
             }, function(err) {
               if (err) { throw err; }
               else {
