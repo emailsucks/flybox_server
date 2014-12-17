a5fb5c76cd2b999b5d8e8d9f73fcf4ec6a73d762
Mark Harrell <mark@mjharrell.com>
2014-12-17 14:34:29 -0800

    multi-part form test for boxes

--- test/api/box_tests.js
+++ test/api/box_tests.js
@@ -6,9 +6,12 @@ var chaihttp = require('chai-http');
 chai.use(chaihttp);
 
 require('../../server');
-
+var FormData = require('form-data');
 var expect = chai.expect;
 var appUrl = 'http://localhost:3000';
+var request = require('request');
+var util = require('util');
+
 
 describe('box routes', function() {
   var jwtToken;
@@ -49,17 +52,35 @@ describe('box routes', function() {
   });
 
   it('should make a box through mailHook', function(done) {
-    chai.request(appUrl)
-    .post('/api/mailHook')
-    .send({mailinMsg: {
-      html: '<h1>this is some html</h1> #to test@example.com #',
-      text: 'this is some text',
-      subject: 'a subject',
-      from: [{address: 'flybox4real@gmail.com', name: 'mr. box'}]
-    }})
-    .end(function(err, res) {
-      expect(err).to.eql(null);
-      done();
+    var form = new FormData();
+    var finalizedMessage = {
+        html: '<h1>this is some html</h1> #to test@example.com #',
+        text: '#to test@example.com test1@example.com #',
+        subject: 'a subject',
+        from: [{address: 'flybox4real@gmail.com', name: 'mr. box'}]
+    };
+
+    form.append('mailinMsg', JSON.stringify(finalizedMessage));
+
+    form.getLength(function(err, length) {
+
+      var headers = form.getHeaders();
+      headers['Content-Length'] = length;
+
+      var r = request.post({
+        url: appUrl + '/api/mailHook/',
+        timeout: 30000,
+        headers: headers
+      }, function(err, resp, body) {
+        var jsonParsed = JSON.parse(resp.body);
+        if (err || resp.statusCode !== 200) {
+          return console.log('error posting to webhook ' + err || resp);
+        }
+        expect(resp.statusCode).to.eql(200);
+        expect(jsonParsed.subject).to.eql('a subject');
+        done();
+      });
+      r._form = form;
     });
   });
 
