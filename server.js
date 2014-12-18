492cfdb23a4976fd4b16470ec6e0de9c8146e905
Mark Harrell <mark@mjharrell.com>
2014-12-18 14:59:05 -0800

    socketio test heroku

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ require('./routes/user_routes')(app, passport, jwtAuth);
 require('./routes/mailHook')(app);
 require('./routes/box_routes')(app, jwtAuth);
 
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-io.sockets.on('connection', socket);
+// var server = require('http').Server(app);
+// var io = require('socket.io')(server);
+// io.sockets.on('connection', socket);
 
 app.listen(port, function() {
   console.log('Server listening on port ' + port);
